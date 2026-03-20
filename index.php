23a38755dfa89fe934d6bd31e0d63a9aee3e418d
cyclist-dev <moc.profissional@gmail.com>
2026-03-19 22:05:41 -0300

    Primeiro commit biblioteca acervo digital

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    include("menu.php");
+    ?>
+    <div class="content">
+        <h1>Biblioteca Digital Municipal</h1>
+        <p>A Biblioteca Digital Municipal em como missãopromover o acesso ao conhecimento,
+             a cultura e a educação por meio de um acervo digital acessível a toda a comunidade.</p>
+             <br>
+             <p>Nosso sistema permite consultar obras literárias, acessar livros digitais e incentivar
+a leitura através da tecnologia.
+            Este ambiente foi desenvolvido para facilitar o gerenciamento do acervo bibliográfico
+e aproximar leitores do universo literário. 
+        </p>
+    </div>
+
+</body>
+
+</html>
